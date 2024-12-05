2fd8d716e92f22db74abddc79cde1813acc4e7df
Matthew Poremba <matthew.poremba@amd.com>
2024-12-05 15:08:17 -0800

    dev-amdgpu: Always claim GPU "ASIC" is initialized
    When multiple GPUs are used, it checks that additional GPUs are done
    initializing hardware. Since this is simulation, claim this is true
    always. This avoids getting stuck at this check.

--- src/dev/amdgpu/amdgpu_nbio.cc
+++ src/dev/amdgpu/amdgpu_nbio.cc
@@ -114,6 +114,9 @@ AMDGPUNbio::readMMIO(PacketPtr pkt, Addr offset)
       case AMDGPU_MP1_SMN_C2PMSG_90:
         pkt->setLE<uint32_t>(0x1);
         break;
+      case MI200_BIOS_SCRATCH_7:
+        pkt->setLE<uint32_t>(0x200); // ATOM_S7_ASIC_INIT_COMPLETE_MASK
+        break;
       default:
         if (triggered_reads.count(offset)) {
             DPRINTF(AMDGPUDevice, "Found triggered read for %#x\n", offset);
